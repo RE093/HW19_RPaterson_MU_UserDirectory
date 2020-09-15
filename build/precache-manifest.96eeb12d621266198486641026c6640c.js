a5eead7df8c3611b4b9160195f2be2d78bcd224d
RE000 <re.paterson000@gmail.com>
2020-09-15 18:12:06 +1000

    added gh-pages dependency to deploy build to github

---
+++ build/precache-manifest.96eeb12d621266198486641026c6640c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e83999e11ccdf965d12285668862c9",
+    "url": "/HW19_RPaterson_MU_UserDirectory/index.html"
+  },
+  {
+    "revision": "a9c1338849176c4fd0f3",
+    "url": "/HW19_RPaterson_MU_UserDirectory/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "14a4cb5a48ab09fbcb4a",
+    "url": "/HW19_RPaterson_MU_UserDirectory/static/css/main.48d436c0.chunk.css"
+  },
+  {
+    "revision": "a9c1338849176c4fd0f3",
+    "url": "/HW19_RPaterson_MU_UserDirectory/static/js/2.e53e3864.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/HW19_RPaterson_MU_UserDirectory/static/js/2.e53e3864.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14a4cb5a48ab09fbcb4a",
+    "url": "/HW19_RPaterson_MU_UserDirectory/static/js/main.09e36a81.chunk.js"
+  },
+  {
+    "revision": "f7ff7cf05bd42f29a06c",
+    "url": "/HW19_RPaterson_MU_UserDirectory/static/js/runtime-main.5de34f73.js"
+  },
+  {
+    "revision": "d96c385b4bd2f2d027d3dc425573a86e",
+    "url": "/HW19_RPaterson_MU_UserDirectory/static/media/banner.d96c385b.jpeg"
+  }
+]);
