2b39c9d4a53477fff6dd3c687ccb43724a774b51
RE000 <re.paterson000@gmail.com>
2020-09-15 17:17:52 +1000

    added jumbotron banner

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,35 +5,42 @@ import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import Dropdown from 'react-bootstrap/Dropdown';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 
 
 export default function Nav(props) {
     return (
-        <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">Employee Directory</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-            <Form inline className="searchBar">
-                <FormControl 
-                    type="text" 
-                    placeholder="Search" 
-                    className="mr-sm-2" 
-                    onChange={props.handleInputChange}
-                    value={props.value}
-                />
-                <Button 
-                    onClick={props.handleFormSubmit} 
-                    variant="outline-success">
-                        Search
-                </Button>
-                <DropdownButton id="dropdown-basic-button" title="Sort By" drop="left">
-                    <Dropdown.Item as="button" value="oldest" onClick={props.handleSortChange}>Age Up</Dropdown.Item>
-                    <Dropdown.Item as="button" value="youngest" onClick={props.handleSortChange}>Age Down</Dropdown.Item>
-                    <Dropdown.Item as="button" value="firstName" onClick={props.handleSortChange}>First Name</Dropdown.Item>
-                    <Dropdown.Item as="button" value={"lastName"} onClick={props.handleSortChange}>Last Name</Dropdown.Item>
-                </DropdownButton>
-            </Form>
-        </Navbar.Collapse>
-        </Navbar>
+        <div>
+            <Jumbotron className="jumbo">
+                <div className="jumboText">
+                <h1>Employee Directory</h1>
+                </div>
+            </Jumbotron >
+            <Navbar bg="light" expand="lg">
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Form inline className="searchBar">
+                    <FormControl 
+                        type="text" 
+                        placeholder="Search" 
+                        className="mr-sm-2" 
+                        onChange={props.handleInputChange}
+                        value={props.value}
+                    />
+                    <Button 
+                        onClick={props.handleFormSubmit} 
+                        variant="outline-success">
+                            Search
+                    </Button>
+                </Form>
+            </Navbar.Collapse>
+            <DropdownButton id="dropdown-basic-button" title="Sort By">
+                        <Dropdown.Item as="button" value="oldest" onClick={props.handleSortChange}>Age Up</Dropdown.Item>
+                        <Dropdown.Item as="button" value="youngest" onClick={props.handleSortChange}>Age Down</Dropdown.Item>
+                        <Dropdown.Item as="button" value="firstName" onClick={props.handleSortChange}>First Name</Dropdown.Item>
+                        <Dropdown.Item as="button" value={"lastName"} onClick={props.handleSortChange}>Last Name</Dropdown.Item>
+                    </DropdownButton>
+            </Navbar>
+        </div>
     );
 }
